d48aa7a665a3dfa2cb4edbcb29d0a6341314372d
alu0101049687 <alu0101049687@ull.edu.es>
2019-03-03 19:10:37 +0000

    .

--- ej-client.js
+++ ej-client.js
@@ -1,8 +1,16 @@
 'use strict';
 const netClient = require('net').connect({port: 60300});
 const ldjClient = require('./lib/ej4.js').connect(netClient);
-ldjClient.on('message', message => { if (message.type === 'watching') {
+ldjClient.on('message', message => { 
+    if (message.type === 'watching') {
 console.log(`Now watching: ${message.file}`); } else if (message.type === 'changed') {
-console.log(`File changed: ${new Date(message.timestamp)}`); } else {
-throw Error(`Unrecognized message type: ${message.type}`); }
+console.log(`File changed: ${new Date(message.timestamp)}`); }
+ 
+else {
+    try {
+        JSON.parse(message);
+    } catch (e) {
+        throw Error(`Unrecognized message type: ${message.type}`);  
+    }
+ }
 });
