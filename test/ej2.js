d43c3ab0efb0af6c8bbc64762aa027eb61ae5b77
alu0101049687 <alu0101049687@ull.edu.es>
2019-03-03 17:10:08 +0000

    .

---
+++ test/ej2.js
@@ -0,0 +1,23 @@
+'use strict';
+const assert = require('assert');
+const EventEmitter = require('events').EventEmitter; 
+const LDJClient = require('../lib/ldj-client2.js');
+
+describe('LDJClient', () => { 
+let stream = null;
+let client = null;
+
+beforeEach(() => {
+stream = new EventEmitter(); 
+})
+
+it('should throw an error when the constructor is null', done => { 
+    try {
+        client = new LDJClient(null);
+      }
+      catch (error) {
+        done();
+      }
+
+});})
+
