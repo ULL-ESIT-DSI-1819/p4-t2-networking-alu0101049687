39033a35be9420d5dd746efa42522d31b244d419
alu0101049687 <alu0101049687@ull.edu.es>
2019-03-05 15:01:38 +0000

    .

--- ej-client.js
+++ ej-client.js
@@ -1,6 +1,6 @@
 'use strict';
 const netClient = require('net').connect({port: 60300});
-const ldjClient = require('./lib/ej4.js').connect(netClient);
+const ldjClient = require('./lib/ej6.js').connect(netClient);
 ldjClient.on('message', message => { 
     if (message.type === 'watching') {
 console.log(`Now watching: ${message.file}`); } else if (message.type === 'changed') {
