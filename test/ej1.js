86045a727bc3f741dcc7057c613d12b85bb63bd2
alu0101049687 <alu0101049687@ull.edu.es>
2019-03-07 20:39:52 +0000

    .

--- test/ej1.js
+++ test/ej1.js
@@ -1,7 +1,7 @@
 'use strict';
 const assert = require('assert');
 const EventEmitter = require('events').EventEmitter; 
-const LDJClient = require('../lib/ldj-client.js');
+const LDJClient = require('../lib/ldj-client3.js');
 
 describe('LDJClient', () => { 
 let stream = null;
@@ -12,12 +12,15 @@ stream = new EventEmitter();
 client = new LDJClient(stream);
 });
 
-it('should emit a message event from a single data event', done => { 
+it('should emit two message event from a single data event', done => { 
     
      client.on('message', message => {
         assert.deepEqual(message, {foo: 'bar'});
-        done(); 
      });
+     client.on('message2', message => {
+      assert.deepEqual(message, {foo: 'bar'});
+      done(); 
+   });
     stream.emit('data', '{"foo":"bar"}\n'); 
     });
 });
