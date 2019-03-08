57b0286652906c57b3a7a4affb1802abc2a234ef
alu0101049687 <alu0101049687@ull.edu.es>
2019-03-08 17:56:51 +0000

    .

--- ldj-client.js
+++
@@ -1,7 +0,0 @@
-constructor(stream) ;{ super();
-    let buffer = ''; stream.on('data', data => {
-    buffer += data;
-    let boundary = buffer.indexOf('\n'); while (boundary !== -1) {
-    const input = buffer.substring(0, boundary); buffer = buffer.substring(boundary + 1); this.emit('message', JSON.parse(input)); boundary = buffer.indexOf('\n');
-    } });
-    };
